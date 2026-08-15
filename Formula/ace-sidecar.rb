7aebc22f1b30aac6c5a3c9b047acb7c3b2d5b2d5
ACE Engineering <contact@acefleet.dev>
2026-08-14 19:56:49 -0700

    chore(release): set initial release version to 0.1.0

--- Formula/ace-sidecar.rb
+++ Formula/ace-sidecar.rb
@@ -3,7 +3,7 @@ class AceSidecar < Formula
 
   desc "Local developer observability sidecar and skill miner for Claude Code & Antigravity"
   homepage "https://github.com/ACE-Engineering/ace-sidecar"
-  url "https://files.pythonhosted.org/packages/source/a/ace-sidecar/ace-sidecar-0.1.1.tar.gz"
+  url "https://files.pythonhosted.org/packages/source/a/ace-sidecar/ace-sidecar-0.1.0.tar.gz"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "AGPL-3.0-or-later"
 
